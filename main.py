40facfb829b7b4a0026da0d79645fc5267fc93d8
Özer, Kaan <kaan.oezer@study.thws.de>
2023-03-30 12:33:58 +0200

    output function is written as a draft

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 
 import markdown
 
+
 # Output Processes
 
-def getOutput(courseName, numberOfStudent):
+def getOutput(courseName, numberOfStudent,date):
 
+    markdownText = f""" 
+# Exam Plan Quality
 
-    markdownText = "#firstCommit"
+## First Exam: 
 
-    return
+### Name of the course: {courseName}  
+### the number of students who enrolled this course: {numberOfStudent}
+
+This exam should be helded on {date} based on this information
+"""
+
+    # converts the markdown text into html
+    html = markdown.markdown(markdownText)
+
+    file_directory = "output.html"
+
+    #creates a file
+    file = open(file_directory,"w",encoding="utf-8")
+
+    file.write(html)
+    print('Printing Process is successfully ended')
+
+    file.close()
+ 
+getOutput('Math',300
+,24.05);
