b6e31f30bf5e5dbf9c6b3105ce130adb2977cb21
Uslu, Müberra Seyma <mueberraseyma.uslu@study.thws.de>
2023-04-30 21:11:44 +0200

    create room dictionary for exams

--- room_distances.py
+++ room_distances.py
@@ -3,6 +3,15 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
+df = pd.read_excel('datafiles/FIW_Exams_2022ws.xlsx')
+
+# Exclude the exams which will be held in the form of online or oral
+df = df[~df['Form'].isin(['muendlich', 'online'])]
+room_df = df[['Lehrveranstaltung', 'HS']]
+
+# Split the exam room names into a list for each row
+room_df['HS'] = room_df['HS'].apply(lambda x: x.split(','))
+
 
 def create_random_distance_matrix(rooms):
     n = len(rooms)
@@ -20,17 +29,33 @@ def create_random_distance_matrix(rooms):
     return distance_matrix
 
 
-def create_sub_matrix(distance_matrix,desired_rooms):
+def create_sub_matrix(distance_matrix, desired_rooms):
     sub_matrix = distance_matrix.loc[desired_rooms, desired_rooms]
     print(sub_matrix)
     return sub_matrix
 
-rooms=["H.1.1","H.1.2","H.1.3","H.1.6","H.1.7","I.2.15","I.3.19",
-       "I.3.20","I.3.24","I.2.1","I.2.15a","I.2.18/19"]
+
+rooms = ["H.1.1", "H.1.2", "H.1.3", "H.1.6", "H.1.7", "H.1.11", "I.2.15", "I.3.19",
+         "I.3.20", "I.3.24", "I.2.1", "I.2.15a", "I.2.18", "I.2.19"]
 
 distance_matrix = create_random_distance_matrix(rooms)
-desired_rooms=["H.1.1","H.1.2","H.1.3","H.1.6"]
-sub_matrix=create_sub_matrix(distance_matrix,desired_rooms)
+
+
+# Create a dictionary which will store the exam names as a key and the exam room/s for each exam as a value of matrix
+def create_room_dic():
+    room_distances = {}
+    for index, row in room_df.iterrows():
+        if len(row['HS']) > 1:  # check if the exam has more than one room -we'll check only them
+            desired_rooms = [room.strip() for room in row['HS']]
+            sub_matrix = create_sub_matrix(distance_matrix, desired_rooms)
+
+            room_distances[row['Lehrveranstaltung']] = sub_matrix
+
+    print(room_distances)
+    return room_distances
+
+
+sub_matrix = create_room_dic()  # just shows the last submatrix (the last exam's rooms and distances)
 
 G = nx.from_pandas_adjacency(distance_matrix)
 subgraph = nx.from_pandas_adjacency(sub_matrix)
@@ -51,6 +76,3 @@ nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=8)
 
 # Show the plot
 plt.show()
-
-
-
