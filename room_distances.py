5d681e0276034892f4fc154953f1f0158224bfc0
Öztürk, Kemal Eren <kemaleren.oeztuerk@study.thws.de>
2023-05-03 13:11:26 +0200

    add room distance matrix and modify room distances.py

--- room_distances.py
+++ room_distances.py
@@ -12,7 +12,16 @@ room_df = df[['Lehrveranstaltung', 'HS']]
 # Split the exam room names into a list for each row
 room_df['HS'] = room_df['HS'].apply(lambda x: x.split(','))
 
+# Distance Matrix
+def read_room_distances():
+    distance_matrix = pd.read_excel('datafiles/room_distance_matrix.xlsx')
+    distance_matrix.index=distance_matrix.columns
+    print(distance_matrix)
+    return distance_matrix
 
+distance_matrix = read_room_distances()
+
+# Random distance generator
 def create_random_distance_matrix(rooms):
     n = len(rooms)
     # create a random distance matrix of size n x n
@@ -25,22 +34,18 @@ def create_random_distance_matrix(rooms):
     distance_matrix = distance_matrix.where(np.triu(np.ones(distance_matrix.shape)).astype(bool))
     distance_matrix = distance_matrix.fillna(distance_matrix.T)
 
-    print(distance_matrix)
+    #print(distance_matrix)
     return distance_matrix
 
 
 def create_sub_matrix(distance_matrix, desired_rooms):
     sub_matrix = distance_matrix.loc[desired_rooms, desired_rooms]
-    print(sub_matrix)
+    #print(sub_matrix)
     return sub_matrix
 
 
-rooms = ["H.1.1", "H.1.2", "H.1.3", "H.1.6", "H.1.7", "H.1.11", "I.2.15", "I.3.19",
-         "I.3.20", "I.3.24", "I.2.1", "I.2.15a", "I.2.18", "I.2.19"]
 
-distance_matrix = create_random_distance_matrix(rooms)
-
-def show_distance_graph(sub_matrix):
+def show_distance_graph(distance_matrix,sub_matrix):
 
     G = nx.from_pandas_adjacency(distance_matrix)
     subgraph = nx.from_pandas_adjacency(sub_matrix)
@@ -75,9 +80,13 @@ def create_room_dic():
 
             # show_distance_graph(sub_matrix)
 
-    print(room_distances)
+    for i in room_distances:
+        print(i)
+        print(room_distances[i])
     return room_distances
 
-
 room_distances = create_room_dic()
-
+print(distance_matrix)
+demo_sub_matrix=create_sub_matrix(distance_matrix,["H.1.1", "H.1.2", "H.1.3","H.1.11"])
+print(demo_sub_matrix)
+show_distance_graph(distance_matrix,demo_sub_matrix)
