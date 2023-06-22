5c48eb2777d8127cbdd812fa55301c38803373a0
Özer, Kaan <kaan.oezer@study.thws.de>
2023-06-23 00:56:24 +0200

    examiners want to come at minimal amounts of days rule added. Randomly generated csv file added. A method that randomly creates special dates added.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import markdown
+from SpecialDates import SpecialDates  # special_dates yerine SpecialDates kullanın
 import pdfkit
 from datetime import datetime
 from Data import Data
@@ -11,13 +12,17 @@ from Scoring import scoring
 from Output import Output
 from Data import data_obj
 
-
 def main():
 
     # Output.save_multiple_result_html()
     # Output.save_result_bigExamEarly()
-    Output.save_result_oneExamPerDay()
+    # Output.save_result_oneExamPerDay()
     # Output.save_result_roomCapacity()
     # Output.save_result_roomDistances()
+
+    special_dates = SpecialDates()  # SpecialDates sınıfından bir örnek oluşturun
+    score, conflicts_df, plot_arr = special_dates.compute()  # compute yöntemini çağırın
+
+    print(score, conflicts_df)  # Sonuçları yazdırın
 
 main()
