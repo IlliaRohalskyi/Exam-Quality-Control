e45c034fc4035325895fb405cff982ba71035484
Uslu, Müberra Seyma <mueberraseyma.uslu@study.thws.de>
2023-06-28 21:33:15 +0200

    data frame is filtered

--- RuleEight.py
+++ RuleEight.py
@@ -7,18 +7,41 @@ class RuleEight:
     def __init__(self):
         self.score, self.conflicts_df, self.plot_arr = self.compute()
 
-    def compute(self):
-        score=None
-        conflicts_df=None
-        plot_arr= None
+    def compute (self):
+        score = None
+        conflicts_df = None
+        plot_arr = None
+
         def specific_examiners():
-            df = pd.concat([data_obj.examiner,data_obj.start_date,data_obj.course_name],axis=1)
+            df = pd.concat([data_obj.examiner, data_obj.start_date, data_obj.course_name], axis=1)
+
+            # data is exploded by putting each professors into seperate rows
             exploded_df = df.explode('1. & 2. Pruefer')
+
+
             grouped_df = exploded_df.groupby('1. & 2. Pruefer').agg(
                 {'Lehrveranstaltung': list, 'start_date': list}).reset_index()
 
+            # data is filtered based on the specific examiners
+            filtered_df = grouped_df[grouped_df['1. & 2. Pruefer'].isin(data_obj.special_examiners['Professor'])]
 
-            print(grouped_df)
+
+            professor_exams = {}
+            for row in filtered_df.itertuples(index=False):
+                professor = row[0]
+                exams = {}
+                for date, exam in zip(row[2], row[1]):
+                    if date in exams:
+                        exams[date].append(exam)
+                    else:
+                        exams[date] = [exam]
+                professor_exams[professor] = exams
+
+            df_final = pd.DataFrame(
+                {'Professor': list(professor_exams.keys()), 'Exams': list(professor_exams.values())})
+
+            print(df_final)
+
         specific_examiners()
         return score, conflicts_df, plot_arr
 
