b763cbc09e9b157fdc8d93a063221c3ef3aa1b6d
Öztürk, Kemal Eren <kemaleren.oeztuerk@study.thws.de>
2023-06-30 10:48:44 +0200

    plot is added in Rule eight

--- RuleEight.py
+++ RuleEight.py
@@ -8,68 +8,85 @@ class RuleEight:
         self.score, self.conflicts_df, self.plot_arr = self.compute()
 
     def compute (self):
-        score = None
-        conflicts_df = None
-        plot_arr = None
 
-        def specific_examiners():
-            df = pd.concat([data_obj.examiner, data_obj.start_date, data_obj.course_name], axis=1)
+        df = pd.concat([data_obj.examiner, data_obj.start_date, data_obj.course_name], axis=1)
 
-            # data is exploded by putting each professors into seperate rows
-            exploded_df = df.explode('1. & 2. Pruefer')
+        # data is exploded by putting each professor into sepa
+        # rate rows
+        exploded_df = df.explode('1. & 2. Pruefer')
 
+        grouped_df = exploded_df.groupby('1. & 2. Pruefer').agg(
+            {'Lehrveranstaltung': list, 'start_date': list}).reset_index()
 
-            grouped_df = exploded_df.groupby('1. & 2. Pruefer').agg(
-                {'Lehrveranstaltung': list, 'start_date': list}).reset_index()
+        # data is filtered based on the specific examiners
+        filtered_df = grouped_df[grouped_df['1. & 2. Pruefer'].isin(data_obj.special_examiners['Professor'])]
 
-            # data is filtered based on the specific examiners
-            filtered_df = grouped_df[grouped_df['1. & 2. Pruefer'].isin(data_obj.special_examiners['Professor'])]
-
-
-            professor_exams = {}
-            for row in filtered_df.itertuples(index=False):
-                professor = row[0]
-                exams = {}
-                for date, exam in zip(row[2], row[1]):
-                    if date in exams:
-                        exams[date].append(exam)
-                    else:
-                        exams[date] = [exam]
-                professor_exams[professor] = exams
-
-            df_final = pd.DataFrame(
-                {'Professor': list(professor_exams.keys()), 'Exams': list(professor_exams.values())})
-
-            def count_days_supervised(exams):
-                dates = list(exams.keys())
-                # Find how many days professors have to visit campus
-                num_days = pd.DataFrame({'date': dates})['date'].dt.day.nunique()
-                return num_days
-
-            def count_exams_supervised(courses):
-                exams = set()
-                for course_list in courses.values():
-                    exams.update(course_list)
-                num_exams = len(exams)
-                return num_exams
-
-            def calculate_each_score(days, number_of_exams):
-                if number_of_exams == 1:
-                    return 1
+        professor_exams = {}
+        for row in filtered_df.itertuples(index=False):
+            professor = row[0]
+            exams = {}
+            for date, exam in zip(row[2], row[1]):
+                if date in exams:
+                    exams[date].append(exam)
                 else:
-                    return 1-((days-1)/(number_of_exams-1))
+                    exams[date] = [exam]
+            professor_exams[professor] = exams
 
+        df_final = pd.DataFrame(
+            {'Professor': list(professor_exams.keys()), 'Exams': list(professor_exams.values())})
+
+        def count_days_supervised(exams):
+            dates = list(exams.keys())
             # Find how many days professors have to visit campus
-            df_final["Days"] = df_final["Exams"].apply(count_days_supervised)
-            # Find how many exams professors have to supervise
-            df_final["Exam Numbers"] = df_final["Exams"].apply(count_exams_supervised)
-            # Calculate score
-            df_final["Score"] = df_final.apply(lambda i: calculate_score(i["Days"], i["Exam Numbers"]),axis=1)
+            num_days = pd.DataFrame({'date': dates})['date'].dt.day.nunique()
+            return num_days
 
-            print(df_final)
+        def count_exams_supervised(courses):
+            exams = set()
+            for course_list in courses.values():
+                exams.update(course_list)
+            num_exams = len(exams)
+            return num_exams
 
-        specific_examiners()
-        return score, conflicts_df, plot_arr
+        def calculate_each_score(days, number_of_exams):
+            if number_of_exams == 1:
+                return 1
+            else:
+                return 1 - ((days - 1) / (number_of_exams - 1))
 
+        # Find how many days professors have to visit campus
+        df_final["Days"] = df_final["Exams"].apply(count_days_supervised)
+        # Find how many exams professors have to supervise
+        df_final["Exam Numbers"] = df_final["Exams"].apply(count_exams_supervised)
+        # Calculate score
+        df_final["Score"] = df_final.apply(lambda row: calculate_each_score(row["Days"], row["Exam Numbers"]), axis=1)
 
-RuleEight = RuleEight()
+        def plot():
+            # Plot the number of exams and days
+            plt.figure(figsize=(10, 6))
+            plt.bar(df_final['Professor'], df_final['Exam Numbers'], label='Number of Exams')
+            plt.bar(df_final['Professor'], df_final['Days'], label='Number of Days')
+
+            x = np.arange(len(df_final))
+
+            # Customize the plot
+            plt.xlabel('Professor')
+            plt.ylabel('Count')
+            plt.title('Number of Exams and Days Supervised')
+            plt.legend()
+
+            # Rotate x-axis labels for better visibility if needed
+            plt.xticks(rotation=45)
+
+            # Display the plot
+            plt.tight_layout()
+            # Get the current figure
+            figure = plt.gcf()
+
+            # Convert the plot to a NumPy array
+            plot_array = np.array(figure.canvas.renderer.buffer_rgba())
+            return plot_array
+
+        overall_score = df_final["Score"].mean()
+
+        return overall_score, df_final, plot()
